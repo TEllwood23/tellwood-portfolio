077e1629a450e42bb477fd734e12765c4ca80748
Thomas Ellwood <taellwood@gmail.com>
2024-05-17 14:34:36 +0100

    fixed error on tailwid config

--- nextjs-frontend/k-h-website/tailwind.config.ts
+++ nextjs-frontend/k-h-website/tailwind.config.ts
@@ -1,20 +1,15 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",    // Include all files in the src/pages directory and subdirectories
+    "./src/components/**/*.{js,ts,jsx,tsx}", // Include all files in the src/components directory and subdirectories
+    "./src/styles/**/*.{css,scss}",       // Include all CSS/SCSS files in the src/styles directory and subdirectories
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+
 export default config;
