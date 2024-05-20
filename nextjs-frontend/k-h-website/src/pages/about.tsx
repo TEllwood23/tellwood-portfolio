b59bcd82028dfc9d2e8c7eab6533abc333473e40
Thomas Ellwood <taellwood@gmail.com>
2024-05-20 10:52:39 +0100

    set up the About page

---
+++ nextjs-frontend/k-h-website/src/pages/about.tsx
@@ -0,0 +1,12 @@
+import Container from '../components/Container';
+
+const AboutPage = () => {
+  return (
+    <Container bgColor="bg-blue-100">
+      <h1>Welcome to My Next.js App</h1>
+      <p>This is the about page.</p>
+    </Container>
+  );
+};
+
+export default AboutPage;
