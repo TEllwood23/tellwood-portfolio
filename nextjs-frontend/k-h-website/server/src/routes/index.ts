47be1967bc3efae92e69b8704b1c5e3dcfb13d74
Thomas Ellwood <taellwood@gmail.com>
2024-05-20 16:28:31 +0100

    fixing errors on front-end structure and rebuilding back-end

--- nextjs-frontend/k-h-website/server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-// k-h-website/server/src/routes/index.ts
-import { Router } from 'express';
-import { getIndex } from '../controllers/indexController';
-
-const router = Router();
-
-router.get('/endpoint', getIndex);
-
-export default router;
