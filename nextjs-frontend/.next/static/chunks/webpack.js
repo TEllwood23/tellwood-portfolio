902e6f682e15f45affd477fade12a06be8c59c52
Thomas Ellwood <taellwood@gmail.com>
2024-05-21 18:32:50 +0100

    set up postgresql database

--- nextjs-frontend/.next/static/chunks/webpack.js
+++ nextjs-frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a96a4d417e7a7ec3"; }
+/******/ 		__webpack_require__.h = function() { return "dacb4bf23e491829"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
