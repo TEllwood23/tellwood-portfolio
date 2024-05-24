79fe46da4c04613341a80d8ed095717aa3de25d5
Thomas Ellwood <taellwood@gmail.com>
2024-05-24 16:04:45 +0100

    added .env.local file to frontend

--- nextjs-frontend/.next/static/chunks/webpack.js
+++ nextjs-frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dacb4bf23e491829"; }
+/******/ 		__webpack_require__.h = function() { return "c73fe18a860aa858"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
