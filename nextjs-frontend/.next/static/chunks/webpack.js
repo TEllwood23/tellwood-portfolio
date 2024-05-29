bba91efb51cb02a06b3c22601baf5e433d6c548f
Thomas Ellwood <taellwood@gmail.com>
2024-05-29 09:47:31 +0100

    fixed font issue with MainContent component

--- nextjs-frontend/.next/static/chunks/webpack.js
+++ nextjs-frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4ead6598455cf172"; }
+/******/ 		__webpack_require__.h = function() { return "51b84fde00736225"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
